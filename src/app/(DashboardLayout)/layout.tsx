cd1c25b5216f2b3e16eb3d145c7bd73f56b3a57a
Md Soriful hasan <sorifullhasan300@gmail.com>
2026-02-11 11:01:24 +0600

    feat(layout): implement role-based dashboard layout - Add role-based conditional rendering for admin, student, and tutor views - Create initial admin dashboard page component - Add user session service to retrieve and determine user role - Include constants file for role enumeration

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { Roles } from "@/constants/constants";
+import { userService } from "@/service/session.service";
+import React, { ReactNode } from "react";
 
-export default function layout() {
-  return <div>layout</div>;
+export default async function layout({
+  admin,
+  student,
+  tutor,
+}: {
+  admin: ReactNode;
+  student: ReactNode;
+  tutor: ReactNode;
+}) {
+  const { data, error } = await userService.getSession();
+  const role = data.user.role;
+  return (
+    <div>
+      {role === Roles.admin && admin}
+      {role === Roles.student && student}
+      {role === Roles.tutor && tutor}
+    </div>
+  );
 }
